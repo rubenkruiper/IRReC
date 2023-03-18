33a46557e632fa7fbd25a7ed04eafbd10f1084d3
Ruben Kruiper <rubenkruiper@gmail.com>
2023-03-18 10:19:30 -0700

    updated settings for test run

--- Classifier/classifier.py
+++ Classifier/classifier.py
@@ -48,21 +48,21 @@ class Classifier:
 
         self.metric = metric
 
-        # todo; pickle the classifier after training, so we can re-use it
-        # todo; pickle the classifier after training, so we can re-use it
-        # todo; pickle the classifier after training, so we can re-use it
-        # todo; pickle the classifier after training, so we can re-use it
-        # todo; pickle the classifier after training, so we can re-use it
-        self.knn_classifier = KNeighborsClassifier(n_neighbors=50,
-                                                   weights='distance',
-                                                   leaf_size=100,
-                                                   metric=metric,
-                                                   n_jobs=4)
+        self.classifier_path = self.embedder.embedding_dir.joinpath("classifier.pkl")
+        if self.classifier_path.exists():
+            self.knn_classifier = pickle.load(open(self.classifier_path, 'rb'))
+        else:
+            self.knn_classifier = KNeighborsClassifier(n_neighbors=50,
+                                                       weights='distance',
+                                                       leaf_size=100,
+                                                       metric=metric,
+                                                       n_jobs=4)
 
     def train_classifier_from_heuristics(self,
                                          classifier_nr_neighbours: int = 500,
                                          min_tfidf_value: float = 0.6,
-                                         min_num_foreground_neighbours: int = 375
+                                         min_num_foreground_neighbours: int = 375,
+                                         recreate: bool = False
                                          ):
         """
 
@@ -70,68 +70,69 @@ class Classifier:
         :param min_tfidf_value:    The minimum value for the TFIDF-based heuristic to determine domain-specificity.
         :param min_num_foreground_neighbours:    The minimum number of neighbours from the foreground corpus for a span
                                                  to be considered within-domain -- the second heuristic.
+        :param recreate:    Whether to recreate the classifier from scratch. todo; currently no value passed here.
         """
-        # check if classifier exists;
-        classifier_path = self.embedder.embedding_dir.joinpath("classifier.pkl")
-        if classifier_path.exists():
-            self.knn_classifier = pickle.load(open(classifier_path, 'rb'))
+        if self.classifier_path.exists() and not recreate:
+            # check if a trained classifier already;
+            self.knn_classifier = pickle.load(open(self.classifier_path, 'rb'))
             self.domain_terms = pickle.load(open(self.embedder.embedding_dir.joinpath('domain_terms.pkl'), 'rb'))
             self.ood_terms = set(pickle.load(open(self.embedder.embedding_dir.joinpath('ood_terms.pkl'), 'rb')))
+        else:
+            # (1) compute the knn graph for the standardised embedding data [(span, embedding), (span, embedding), ...]
+            knn_graph = kneighbors_graph(self.standardised_embedding_data,
+                                         classifier_nr_neighbours,
+                                         metric=self.metric,
+                                         n_jobs=8)
 
-        # (1) compute the knn graph for the standardised embedding data [(span, embedding), (span, embedding), ...]
-        knn_graph = kneighbors_graph(self.standardised_embedding_data,
-                                     classifier_nr_neighbours,
-                                     metric=self.metric,
-                                     n_jobs=8)
+            # (2) compute the number of background neighbours and foreground neighbours as a feature
+            span_df_dict = {}
+            for span_idx, (span, _) in enumerate(self.standardised_embedding_data):
+                number_of_background_corpus_neighbours = 0
+                for neighbour_idx in knn_graph[span_idx].indices:
+                    if self.standardised_embedding_data[neighbour_idx][0] in self.ood_terms:
+                        number_of_background_corpus_neighbours += 1
 
-        # (2) compute the number of background neighbours and foreground neighbours as a feature
-        span_df_dict = {}
-        for span_idx, (span, _) in enumerate(self.standardised_embedding_data):
-            number_of_background_corpus_neighbours = 0
-            for neighbour_idx in knn_graph[span_idx].indices:
-                if self.standardised_embedding_data[neighbour_idx][0] in self.ood_terms:
-                    number_of_background_corpus_neighbours += 1
+                span_df_dict[span] = {
+                    'span_idx': span_idx,
+                    'num_background_neighbours': number_of_background_corpus_neighbours,
+                    'num_foreground_neighbours': classifier_nr_neighbours - number_of_background_corpus_neighbours
+                }
 
-            span_df_dict[span] = {
-                'span_idx': span_idx,
-                'num_background_neighbours': number_of_background_corpus_neighbours,
-                'num_foreground_neighbours': classifier_nr_neighbours - number_of_background_corpus_neighbours
-            }
+            # (3) compute a TF-IDF style feature, inspired by Meyers et al. (2018)
+            cleaned_foreground_terms_c = Counter(self.foreground_terms)
+            cleaned_background_terms_c = Counter(self.background_terms)
 
-        # (3) compute a TF-IDF style feature, inspired by Meyers et al. (2018)
-        cleaned_foreground_terms_c = Counter(self.foreground_terms)
-        cleaned_background_terms_c = Counter(self.background_terms)
+            for span, _ in self.standardised_embedding_data:
+                foreground_cnt = cleaned_foreground_terms_c[span]
+                background_cnt = cleaned_background_terms_c[span]
+                TF_fore_back = np.log(1 + (foreground_cnt / (foreground_cnt + background_cnt)))
 
-        for span, _ in self.standardised_embedding_data:
-            foreground_cnt = cleaned_foreground_terms_c[span]
-            background_cnt = cleaned_background_terms_c[span]
-            TF_fore_back = np.log(1 + (foreground_cnt / (foreground_cnt + background_cnt)))
+                tokens, indices = self.embedder.prepare_tokens(span)
+                idf_weights = self.embedder.get_idf_weights_for_indices(tokens, indices)
+                TFIDF_fore_back = TF_fore_back * np.log(np.mean(idf_weights))
 
-            tokens, indices = self.embedder.prepare_tokens(span)
-            idf_weights = self.embedder.get_idf_weights_for_indices(tokens, indices)
-            TFIDF_fore_back = TF_fore_back * np.log(np.mean(idf_weights))
+                span_df_dict[span]['TFIDF_fore_back'] = TFIDF_fore_back
 
-            span_df_dict[span]['TFIDF_fore_back'] = TFIDF_fore_back
+            # (4) Pandas DF with all features
+            span_features_df = pd.DataFrame.from_dict(span_df_dict, orient='index')
+            domain_terms_df = span_features_df[(span_features_df['TFIDF_fore_back'] >= min_tfidf_value) & (
+                              span_features_df['num_foreground_neighbours'] >= min_num_foreground_neighbours)]
+            ood_terms_df = span_features_df[(span_features_df['TFIDF_fore_back'] < min_tfidf_value) | (
+                           span_features_df['num_foreground_neighbours'] < min_num_foreground_neighbours)]
 
-        # (4) Pandas DF with all features
-        span_features_df = pd.DataFrame.from_dict(span_df_dict, orient='index')
-        domain_terms_df = span_features_df[(span_features_df['TFIDF_fore_back'] >= min_tfidf_value) & (
-                          span_features_df['num_foreground_neighbours'] >= min_num_foreground_neighbours)]
-        ood_terms_df = span_features_df[(span_features_df['TFIDF_fore_back'] < min_tfidf_value) | (
-                       span_features_df['num_foreground_neighbours'] < min_num_foreground_neighbours)]
+            # (5) write the domain and Out Of Domain (ood) terms to file, store in embeddings directory
+            self.domain_terms = list(set(domain_terms_df.index.tolist()))
+            self.ood_terms = list(set(ood_terms_df.index.tolist()))
+            pickle.dump(self.domain_terms, open(self.embedder.embedding_dir.joinpath('domain_terms.pkl'), 'wb'))
+            pickle.dump(self.ood_terms, open(self.embedder.embedding_dir.joinpath('ood_terms.pkl'), 'wb'))
 
-        # (5) write the domain and Out Of Domain (ood) terms to file, store in embeddings directory
-        self.domain_terms = list(set(domain_terms_df.index.tolist()))
-        self.ood_terms = list(set(ood_terms_df.index.tolist()))
-        pickle.dump(self.domain_terms, open(self.embedder.embedding_dir.joinpath('domain_terms.pkl'), 'wb'))
-        pickle.dump(self.ood_terms, open(self.embedder.embedding_dir.joinpath('ood_terms.pkl'), 'wb'))
+            # (6) prepare training data for the classifier
+            knn_spans, knn_X = zip(*self.standardised_embedding_data)
+            knn_y = ['y' if s in self.domain_terms else 'n' for s in knn_spans]
 
-        # (6) prepare training data for the classifier
-        knn_spans, knn_X = zip(*self.standardised_embedding_data)
-        knn_y = ['y' if s in self.domain_terms else 'n' for s in knn_spans]
-
-        # set up the classifier and save it
-        self.knn_classifier.fit(knn_X, knn_y)
+            # set up the classifier and save it
+            self.knn_classifier.fit(knn_X, knn_y)
+            pickle.dump(open(self.classifier_path, 'wb'))
 
     def predict_domains(self, spans_to_predict: Union[List[str], str] = None) -> List[str]:
         """
@@ -151,7 +152,7 @@ class Classifier:
         return domain_spans
 
     def prep_semantic_similarity(self):
-        """
+        """ # todo; deprecated // not used
         Prepares a dictionary that holds the top_k nearest neighbours for each span that is present in (1) the
         domain spans extracted from the foreground corpus, and (2) any domain-spans found in definitions of the KG.
         """
@@ -167,7 +168,7 @@ class Classifier:
             self.knn_sim_dict[span] = [unique_spans[neighbour_idx] for neighbour_idx in knn_graph[span_idx].indices]
 
     def get_semantically_similar_terms(self, span_list: Union[List[str], str] = None) -> List[List[str]]:
-        """
+        """ # todo; deprecated // not used
         Simply get the `self.top_k_semantic_similarity` neighbours for each span in a list. Note that for a span to be
         assigned a set of semantically similar span, it has to have been considered a domain term in our classifier. If
         the span does not occur in `self.knn_sim_dict`, then it will be assigned an empty list of neighbours.
@@ -188,6 +189,10 @@ class Classifier:
         return neighbours_lists
 
     def prep_nearest_neighbours(self):
+        """
+        We will be computing the NNs from scratch every re-initialization of this classifier; since that means the
+        settings may have changed and, overall, it doesn't take too long.
+        """
         all_embedding_data = self.embedder.standardised_embedding_data + self.embedder.new_embedding_data
         self.unique_spans, unique_embeddings = zip(*all_embedding_data)
         stacked_embeddings = np.stack([np.mean(e, axis=0) if len(e.shape) > 1 else e for e in unique_embeddings])
