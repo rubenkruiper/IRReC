6d0e25c28d5597d78b9be95e7f33f5649807f00d
Ruben Kruiper <rubenkruiper@gmail.com>
2023-03-26 11:56:03 -0700

    solved the pydantic related bug

--- Classifier/classifier_api.py
+++ Classifier/classifier_api.py
@@ -58,7 +58,7 @@ class Settings(BaseSettings):
 
 class ListToPredict(BaseModel):
     span_lists: List[List[str]]
-    spans = None
+    spans: Union[List[List[str]], str] = None
 
 
 class ToPredict(BaseModel):
